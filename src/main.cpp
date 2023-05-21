89b2c9317763c6b17020ef2fe5fb2dd8ddb47126
Cesar Mello <cmello@gmail.com>
2023-05-20 17:07:51 -0700

    Add file async write

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include <asio.hpp>
+#include <vector>
 
-//#include <asio/co_spawn.hpp>
-//#include <asio/ip/tcp.hpp>
+#include <asio.hpp>
+#include <asio/experimental/as_tuple.hpp>
+#include <asio/experimental/awaitable_operators.hpp>
 
 using asio::io_context;
 using asio::awaitable;
@@ -10,39 +11,114 @@ using asio::co_spawn;
 using asio::ip::tcp;
 using asio::use_awaitable;
 using asio::detached;
+using namespace asio::experimental::awaitable_operators;
+
 namespace this_coro = asio::this_coro;
 
 using namespace std;
 
-awaitable<void> timer_test()
+// waits asynchronously until the specified time elapses.
+awaitable<void> async_wait(std::chrono::steady_clock::duration duration)
 {
-    auto executor = co_await this_coro::executor;
-
-    for (int i = 0; i < 10; i++)
-    {
-        cout << "Waiting event " << i << endl;
-
-        asio::steady_timer t(executor, 2s);
-
-        co_await t.async_wait(asio::use_awaitable);
-    }
+    asio::steady_timer timer(co_await this_coro::executor);
+    timer.expires_after(duration);
+    co_await timer.async_wait(asio::use_awaitable);
 }
 
+// aynchonously buffers data until 2 MB block is filled or caller requests flush explicitly.
+// NOTE: couldn't make buffered_write_stream work with stream_file yet, as soon as that works the code below can be removed.
+template<class input_buffer_type>
+class buffered_file
+{
+
+public:
+    buffered_file(asio::any_io_executor& executor, const char* path)
+        : _file(executor, path, 
+            asio::stream_file::create
+            | asio::stream_file::read_write
+            | asio::stream_file::sync_all_on_write)
+    {
+
+    }
+
+    awaitable<void> write_async(input_buffer_type& input_buffer, size_t count)
+    {
+        // ugly unnecessary memory allocation, until move to asio::buffered_write_stream
+        _buffer.insert(_buffer.end(), input_buffer.begin(), input_buffer.begin() + count);
+
+        if (_buffer.size() > _max_buffer_size)
+        {
+            co_await flush_async();
+        }
+    }
+
+    awaitable<void> flush_async()
+    {
+        if (_buffer.empty())
+        {
+            cout << "nothing to write" << endl;
+            co_return;
+        }
+
+        cout << "writing " << _buffer.size() << endl;
+
+        co_await asio::async_write(_file, asio::buffer(_buffer), asio::use_awaitable);
+        _buffer.clear();
+    }
+
+private:
+    std::vector<char> _buffer;
+    asio::stream_file _file;
+    const size_t _max_buffer_size = 2 * 1024 * 1024;
+};
+
+// data receive loop.
 awaitable<void> receiver()
 {
     auto executor = co_await this_coro::executor;
 
-    for (;;)
+    typedef std::array<char, 64 * 1024> receive_buffer_type;
+    receive_buffer_type receive_buffer;
+    buffered_file<receive_buffer_type> buffered_file(executor, "udp_traffic.bin");
+
+    while(true)
     {
         asio::ip::udp::endpoint sender_endpoint;
 
-        constexpr int buffer_size = 64 * 1024;
-        char data[buffer_size];
-
         auto socket = asio::ip::udp::socket(executor, asio::ip::udp::endpoint(asio::ip::udp::v4(), 20777));
-        size_t received_count = co_await socket.async_receive_from(asio::buffer(data), sender_endpoint, asio::use_awaitable);
+        
+        // continues after either of the events below happen (|| operator implements that composition of async events)
+        auto result = co_await(
+            socket.async_receive_from(asio::buffer(receive_buffer), sender_endpoint, asio::use_awaitable) // either received data...
+            || async_wait(5s)                                                                             // ... or 2s elapsed with no data
+        );
 
-        cout << "received: " << received_count << endl;
+        try
+        {
+
+            switch (result.index())
+            {
+                case 0:
+                {
+                    // first event happened: received data. add to disk buffer until we have enough to fill a disk write.
+                    auto received_count = std::get<0>(result);
+                    //cout << "received: " << received_count << endl;
+                    co_await buffered_file.write_async(receive_buffer, received_count);
+                    break;
+                }
+                case 1:
+                {
+                    // second event happened: time elapsed without receiving data. let's flush to disk.
+                    cout << "flushing" << endl;
+                    co_await buffered_file.flush_async();
+                    break;
+                }
+            }
+        }
+        catch (exception& e)
+        {
+            cout << "unhandled Exception: " << e.what() << endl;
+        }
     }
 }
 
@@ -53,8 +129,7 @@ int main()
         cout << "Binding UDP port..." << endl;
 
         io_context ctx;
-
-        co_spawn(ctx, timer_test(), detached);
+        auto work_guard = asio::make_work_guard(ctx); // keeps the executor loop even if there is no more pending work.
 
         co_spawn(ctx, receiver(), detached);
 
